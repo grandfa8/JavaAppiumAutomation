56c4ce8141d620d0eca31224e83ee64632c211d8
b.asvarov <grandfa8@gmail.com>
2021-09-12 22:47:10 +0300

    lesson 1-2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,21 +1,29 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class MainClassTest extends MainClass {
     @Test
-    public void testGetLocalNumber() {
-        int actual = this.getLocalNumber();
-        int expected = 14;
+    public void testGetClassNumber() {
+        int actual = this.getClassNumber();
+        int expected = 45;
 
-        Assert.assertEquals(
-                this.notEqualsNumberMessage(actual, expected),
-                expected,
-                actual
+        Assert.assertTrue(
+                this.failedGreaterThanNumberMessage(actual, expected),
+                greaterThan(actual, expected)
         );
     }
 
-    private String notEqualsNumberMessage(int actual, int expected) {
-        String msgTemplate = "Actual number '%s' not equal expected number '%s'";
+    private String failedGreaterThanNumberMessage(int actual, int expected) {
+        String msgTemplate = "Actual number '%s' less or equal than expected number '%s'";
         return String.format(msgTemplate, actual, expected);
+    }
+
+    private boolean greaterThan(int actual, int expected) {
+        if (actual > expected) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
