8b2d09a2189cb9b4ef9ac0f477ddef56b668963e
grandfa8 <grandfa8@gmail.com>
2022-01-16 17:54:55 +0300

    more simple

--- src/main/java/MainClassTest.java
+++ src/main/java/MainClassTest.java
@@ -5,31 +5,9 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
     @Test
     public void testGetClassString() {
-        String actual = this.getClassString();
-        String expected = "hello|Hello";
-
         Assert.assertTrue(
-                failedContainsSubstringMessage(actual, expected),
-                stringContains(actual, expected)
+                "GetClassString don't have 'Hello' or 'hello'.",
+                this.getClassString().contains("hello") || this.getClassString().contains("Hello")
         );
-    }
-
-    private String failedContainsSubstringMessage(String actual, String expected) {
-        String msgTemplate = "Actual string '%s' does not contain at least one of the expected substring: '%s'";
-        return String.format(msgTemplate, actual, expected);
-    }
-
-    private boolean stringContains(String actual, String expected) {
-        String[] substrings = expected.split("\\|");
-
-        boolean result = false;
-        for (String substring : substrings) {
-            System.out.println(substring);
-            result = actual.contains(substring);
-            if (result) {
-                break;
-            }
-        }
-        return result;
     }
 }
